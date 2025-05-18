a12093758c49fcc0d4cedd56f78396725a3e3077
rrithick <rithickchandranramesh@gmail.com>
2025-05-18 19:51:03 +0530

    app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from PIL import Image
+import tensorflow
 
 # Configuration
 IMG_HEIGHT = 128
